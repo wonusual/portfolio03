49b237d51176cea3a08a83f98b528edf8a852cbf
wonusual <80879857+wonusual@users.noreply.github.com>
2021-06-07 15:34:51 +0900

    Add files via upload

---
+++ script/script.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+
+    $(".hamburg").click(function () {
+
+        $(".hamburg").toggleClass("active");
+        $(".mobile-menu").toggleClass("active");
+        $(".toggle-logo").toggleClass("active");
+        $(".header-logo").toggleClass("active");
+
+    });
+
+});
